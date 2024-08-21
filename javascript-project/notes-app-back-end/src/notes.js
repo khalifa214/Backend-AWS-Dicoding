fda4a7d393a9fbfb80d2c8a43a185efa0e99c9d2
Ahmad_Khalifa <ahmad.laa007@gmail.com>
2024-08-21 13:01:52 +0700

    latihan membuat node app backend

---
+++ javascript-project/notes-app-back-end/src/notes.js
@@ -0,0 +1,12 @@
+const notes = [
+    {
+        id: '1',
+        title: 'tes',
+        tags: ['tag1', 'tag2'],
+        body: 'tes body',
+        createdAt: '',
+        updatedAt: '',
+    }
+];
+
+module.exports = notes;
